943c02b743d47a4cb557ecc0f0d6d2a2edc4e4c5
davidvk89 <99416738+davidvk89@users.noreply.github.com>
2022-09-09 18:04:10 +0200

    Update payment-update-functions.js
    moved function to admin/dashboard.php

--- admin/lib/js/payment-update-functions.js
+++ admin/lib/js/payment-update-functions.js
@@ -2,21 +2,7 @@ const paymentID = document.getElementById('paymentsID').name;
 $.post("lib/paymentsUpdateHandler.php",
 {
   id: paymentID
-  
 },
 function(data, status){
-  console.log("Data: " + data + "\nStatus: " + status);
   document.getElementById('paymentWindow').innerHTML = data;
-})
-
-setInterval(function () {
-$.post("lib/paymentsUpdateHandler.php",
-{
-  id: paymentID
-  
-},
-function(data, status){
-  //DEBUG: console.log("Data: " + data + "\nStatus: " + status);
-  document.getElementById('paymentWindow').innerHTML = data;
-})
-}, 10000);
+});
